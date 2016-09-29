f53778dfad2f04c78d32b2fbf6dd8cb9c41f0f04
Piyush Makhija <piyushmakhija@users.noreply.github.com>
2016-09-30 00:14:54 +0530

    created a new component and added it to the app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component.ts';
+
 @NgModule({
-	imports: [ BrowserModule ]
+	imports: [ BrowserModule ],
+	declarations: [ AppComponent ],
+	bootstrap: [ AppComponent ]
 })
 
 export class AppModule {}
