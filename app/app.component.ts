9a4f531fffc157fa73e7628c43759f0f5e01daa1
Piyush Makhija <piyushmakhija@users.noreply.github.com>
2016-10-05 23:27:11 +0530

    Implemented hero service to fetch heroes from mock heroes array

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,35 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import {Hero} from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import {HeroService} from './hero.service'
 
 @Component({
 	selector: "my-app",
 	templateUrl: 'app/app.component.html',
-	styleUrls: ['app/app.component.css']
+	styleUrls: ['app/app.component.css'],
+	providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
+	constructor(private heroService: HeroService) { }
+
 	title = 'Tour of Heroes';
-	heroes = HEROES;
+	heroes:Hero[];
 	selectedHero: Hero;
 	hero: Hero = {
 		id: 1,
 		name: 'Windstorm'
 	};
+	ngOnInit(): void {
+		this.getHeroes();
+	}
 	onSelect(hero: Hero): void {
 		this.selectedHero = hero;
+	}
+	getHeroes(): void {
+		this.heroes = this.heroService.getHeroes();
 	}
 }
