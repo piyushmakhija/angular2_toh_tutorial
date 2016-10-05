48b6e025728e167106cd5ecb66a92ee77794afd1
Piyush Makhija <piyushmakhija@users.noreply.github.com>
2016-10-05 21:18:47 +0530

    Properties creted in AppComponent to test interpolation 1. html template of app component now in a separate file. 2. created hero object in app component file as a separate class.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+	id: number;
+	name: string;
+}
+
 @Component({
 	selector: "my-app",
-	template : "<h1> My first angular app </h1>"
+	templateUrl: 'app/app.component.html'
 })
 
-export class AppComponent {}
+export class AppComponent {
+	title = 'Tour of Heroes';
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
+}
