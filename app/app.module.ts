6b04af6effbdef0937faac624f7461e26df6d32d
Piyush Makhija <piyushmakhija@users.noreply.github.com>
2016-10-07 03:16:02 +0530

    added simulated web api to fetch hero details and also added methods in hero service to update hero list 1. simulated web api using angular in memory web api 2. replaced mock heroes with the web api 3. implemented http methods - get, put, post, delete 4. updated heroes component and hero detail component to accomodate the http methods

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
+
+// Imports for loading & configuring the in-memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 import { HeroService } from './hero.service'
 
@@ -15,7 +20,9 @@ import { routing } from './app.routing';
 	imports: [
 		BrowserModule,
 		FormsModule,
-		routing
+		routing,
+		HttpModule,
+		InMemoryWebApiModule.forRoot(InMemoryDataService)
 	],
 	declarations: [
 		AppComponent,
