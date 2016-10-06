63a7ee804cec2a9935f716476098e702deb2fe03
Piyush Makhija <piyushmakhija@users.noreply.github.com>
2016-10-06 19:25:50 +0530

    modified dashboard component and integrated hero detail with the router 1. dashboard modified to include top four heroes. 2. hero detail component integrated with router using route parameter as hero id

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,12 +1,35 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 import {Hero} from './hero';
+
+import { HeroService } from './hero.service';
 
 @Component({
 	selector: 'my-hero-detail',
 	templateUrl: 'app/hero-detail.component.html'
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+
+	constructor(
+		private route: ActivatedRoute,
+		private heroService: HeroService,
+		private location: Location
+	) { }
+
 	@Input()
 	hero: Hero;
+
+	ngOnInit(): void {
+		this.route.params.forEach((params: Params) => {
+			let id = +params['id'];
+			this.heroService.getHero(id)
+				.then(hero => this.hero = hero);
+		})
+	}
+
+	goBack(): void {
+		this.location.back();
+	}
 }
