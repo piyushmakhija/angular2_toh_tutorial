3299195ff10b242bf4563aadc1f67ef09400daf7
Piyush Makhija <piyushmakhija@users.noreply.github.com>
2016-10-06 19:48:53 +0530

    css stylesheets added to app component, hero-detail, dashboard

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -7,8 +7,10 @@ import {Hero} from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
+	moduleId: module.id,
 	selector: 'my-hero-detail',
-	templateUrl: 'app/hero-detail.component.html'
+	templateUrl: 'hero-detail.component.html',
+	styleUrls: ['hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
 
